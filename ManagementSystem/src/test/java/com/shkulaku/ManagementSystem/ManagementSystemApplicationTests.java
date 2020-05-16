67229a43c8bbaa4cad226eba3c1338a4c553c483
jonah <jonah@DESKTOP-E0IGJM9>
2020-05-16 18:47:50 +0200

    Created JUnit Test for app. Closes #5.

--- ManagementSystem/src/test/java/com/shkulaku/ManagementSystem/ManagementSystemApplicationTests.java
+++ ManagementSystem/src/test/java/com/shkulaku/ManagementSystem/ManagementSystemApplicationTests.java
@@ -1,13 +1,62 @@
 package com.shkulaku.ManagementSystem;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.shkulaku.ManagementSystem.entities.Task;
+import com.shkulaku.ManagementSystem.entities.User;
+import com.shkulaku.ManagementSystem.services.TaskService;
+import com.shkulaku.ManagementSystem.services.UserService;
 
 @SpringBootTest
 class ManagementSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
+	@Autowired
+    private UserService userService;
+    @Autowired
+    private TaskService taskService;
+     
+    @Before(value = "")
+    public void initDb() {
+  	  {
+  		  User newUser = new User("testUser@mail.com", "testUser", "123456");
+  		  userService.createUser(newUser); 
+  	  }
+  	  {
+  		  User newUser = new User("testAdmin@mail.com", "testAdmin", "123456");
+  		  userService.createUser(newUser); 
+  	  }
+  	  
+  	  Task userTask = new Task("03/01/2018", "00:11", "11:00", "You need to work today");
+  	  User user = userService.findOne("testUser@mail.com");
+  	  taskService.addTask(userTask, user);  
+    }
+	 
+    @Test
+    public void testUser() {
+  	  User user = userService.findOne("testUser@mail.com");
+        assertNotNull(user);
+  	  User admin = userService.findOne("testAdmin@mail.com");
+  	  assertEquals(admin.getEmail(),"testAdmin@mail.com");
+    }
+    
+    @Test
+    public void testTask() {
+  	  User user = userService.findOne("testUser@mail.com");
+  	  List<Task> task = taskService.findUserTask(user);
+  	  assertNotNull(task); 
+
+    }
+    
+    
+    
 
 }
